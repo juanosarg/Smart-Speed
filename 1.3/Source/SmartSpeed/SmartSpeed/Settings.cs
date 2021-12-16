9d94d1742fd7c4f07460742e2c685ec2990647dc
giraffe176 <giraffe176@gmail.com>
2021-12-15 20:19:28 -0600

    Update to Settings.cs
    Include changes to add in options to speed up the map even further, and return three and four speed back to their defaulted values.

--- 1.3/Source/SmartSpeed/SmartSpeed/Settings.cs
+++ 1.3/Source/SmartSpeed/SmartSpeed/Settings.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using Verse;
 using System.Collections.Generic;
 
@@ -6,21 +6,25 @@ namespace SmartSpeed.Detouring
 {
     public class Settings : ModSettings
     {
-        //Variables used later on
+        //Default Variables used later on
         public static float onespeed = 1f;
         public static float twospeed = 3f;
-        public static float threespeed = 5f;
-        public static float fourspeed = 6f;
+        public static float threespeed = 6f;
+        public static float fourspeed = 15f;
+        public static bool Nomapselected = true;
+        public static bool Nothinghappening = true;
 
-       
+
         //Saving the Data to an .xml for future retreval
         public override void ExposeData()
         {
             base.ExposeData();
             Scribe_Values.Look(ref onespeed, "onespeed", 1f);
             Scribe_Values.Look(ref twospeed, "twospeed", 3f);
-            Scribe_Values.Look(ref threespeed, "threespeed", 5f);
-            Scribe_Values.Look(ref fourspeed, "fourspeed", 6f);
+            Scribe_Values.Look(ref threespeed, "threespeed", 6f);
+            Scribe_Values.Look(ref fourspeed, "fourspeed", 15f);
+            Scribe_Values.Look(ref Nomapselected, "Nomapselected", true);
+            Scribe_Values.Look(ref Nothinghappening, "Nothinghappening", true);
         }
 
 
@@ -41,13 +45,15 @@ namespace SmartSpeed.Detouring
             listingStandard.ColumnWidth = canvas.width;
             listingStandard.Begin(canvas);
             listingStandard.Label("Settings.onespeed".Translate(Settings.onespeed.SecondsToTicks()));
-            Settings.onespeed = listingStandard.Slider(Settings.onespeed, .1f, 4f);
+            Settings.onespeed = listingStandard.Slider(Settings.onespeed, .1f, 2f);
             listingStandard.Label("Settings.twospeed".Translate(Settings.twospeed.SecondsToTicks()));
             Settings.twospeed = listingStandard.Slider(Settings.twospeed, 1f, 6f);
             listingStandard.Label("Settings.threespeed".Translate(Settings.threespeed.SecondsToTicks()));
             Settings.threespeed = listingStandard.Slider(Settings.threespeed, 3f, 10f);
             listingStandard.Label("Settings.fourspeed".Translate(Settings.fourspeed.SecondsToTicks().ToString()));
-            Settings.fourspeed = listingStandard.Slider(Settings.fourspeed, 5f, 15f);
+            Settings.fourspeed = listingStandard.Slider(Settings.fourspeed, 5f, 20f);
+            listingStandard.CheckboxLabeled("Speed up map if one is not active", ref Settings.Nomapselected, "This will speed up the map significantly on faster speeds when one is not selected");
+            listingStandard.CheckboxLabeled("Speed up map if nothing is happening", ref Settings.Nothinghappening, "This will speed up the map significantly on faster speeds  when nothing is happening in game");
             listingStandard.End();
             base.DoSettingsWindowContents(canvas);
         }
